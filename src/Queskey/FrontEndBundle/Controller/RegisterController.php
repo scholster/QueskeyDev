36d16acccb177659429afce84b81a71d9755facd
Anuj Agarwal <anujagarwal31@yahoo.com>
2014-02-08 11:42:02 +0530

    Revert "efbdhcj"
    This reverts commit 44932aedba44f8616186faf5455a19fbf3ae072e.

--- src/Queskey/FrontEndBundle/Controller/RegisterController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Queskey\FrontEndBundle\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-class RegisterController extends Controller
-{
-}
--- src/Queskey/FrontEndBundle/Tests/Controller/RegisterControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Queskey\FrontEndBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class RegisterControllerTest extends WebTestCase
-{
-}
